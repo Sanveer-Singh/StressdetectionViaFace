060629f3b14ca69d0eba1e8fb9b56d024d5b97cf
Sanveer-Singh <sanveer001@gmail.com>
2020-09-08 07:12:21 +0200

    HSV image and point classes done

---
+++ Facedetection/HSVimage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StressdetectionViaFace.Facedetection
+{
+    
+
+    class HSVimage
+    {
+        // these are the things that make up an HSV image
+        // arrays are in x,y format
+        public double[,] hue;
+        public double[,] saturation;
+        public double[,] lightness;// used for convenience
+        public int width;
+        public int height; 
+
+        public double GetHueAtxy(int x, int y)
+        {
+            double copy1 = hue[x, y];
+            return copy1;
+        }
+        public double GetSaturationAtxy(int x, int y)
+        {
+            double copy1 = saturation[x, y];
+            return copy1;
+        }
+        public double GetLightnessAtxy(int x, int y)
+        {
+            double copy1 = lightness[x, y];
+            return copy1;
+        }
+
+        public HSVimage(Bitmap bmp)
+        {
+            // get the width and height 
+            width = bmp.Width;
+            height = bmp.Height;
+
+            //  set up the arrays 
+            hue = new double[width, height];
+            lightness  = new double[width, height];
+            saturation  = new double[width, height];
+            int x,y ;
+            // loop verticaly 
+            for(y= 0; y<= height; y++)
+            {
+                // loop horizontaly 
+                for (x = 0; x <= width; x++)
+                {
+                    //  fil hue array
+                    hue[x, y] = (Color.FromArgb(bmp.GetPixel(x, y).R, bmp.GetPixel(x, y).G, bmp.GetPixel(x, y).B)).GetHue();
+
+                    // fill lightness array 
+                    lightness [x,y] = (Color.FromArgb(bmp.GetPixel(x, y).R, bmp.GetPixel(x, y).G, bmp.GetPixel(x, y).B)).GetBrightness();
+                    // fill saturation array
+                    saturation [x,y] = (Color.FromArgb(bmp.GetPixel(x, y).R, bmp.GetPixel(x, y).G, bmp.GetPixel(x, y).B)).GetBrightness();
+
+
+                }
+            }
+           
+
+        }
+
+    }
+}
---
+++ utilities/point.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StressdetectionViaFace.utilities
+{
+    class point
+    {
+        // points are just an x and y value 
+        private  int _X;
+        private  int _Y;
+
+       // constructor 
+       public point(int x, int y)
+        {
+            _X = x;
+            _Y = y;
+        }
+
+        // getters and setters 
+
+        public int GetX()
+        {
+            return _X;
+        }
+        public  int GetY()
+        {
+            return _Y;
+        }
+        public void  SetX( int X)
+        {
+          _X = X  ;
+        }
+        public void SetY(int Y)
+        {
+            _Y = Y;
+        }
+        // distance calculator (euclidian)
+        public double GetEuclidDistAtoB(point A, point B)
+        {
+            double dist = 0;
+            dist = Math.Sqrt(Math.Pow((B.GetX() - A.GetX()), 2) + Math.Pow((B.GetY() - A.GetY()), 2));
+            return dist;
+        }
+
+    }
+}

b47baae56a16396b3d3d5aacfb5be5dc2f13e3fe
Sanveer-Singh <sanveer001@gmail.com>
2020-10-06 18:10:27 +0200

    Beta ready

--- Preprocessing/CircularMeanFilter.cs
+++ Preprocessing/CircularMeanFilter.cs
@@ -1,29 +1,22 @@
-﻿using Emgu.CV.CvEnum;
-using Emgu.CV.Structure;
-using StressdetectionViaFace.utilities;
+﻿using StressdetectionViaFace.utilities;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms.VisualStyles;
-using System.Xml.Schema;
 
-namespace StressdetectionViaFace.LBPvariants
+namespace StressdetectionViaFace.Preprocessing
 {
-    class AMLBP
+    class CircularMeanFilter
     {
-
-        // we need a bitmap 
         Bitmap ThisPic;
         // we need a radius 
         int Radius;
         // integer latice 
         List<int> Lattice;
 
-        public AMLBP(Bitmap bmp, int Rad = 3)
+        public CircularMeanFilter(Bitmap bmp, int Rad = 3)
         {
             ThisPic = bmp;
             Radius = Rad;
@@ -35,10 +28,9 @@ namespace StressdetectionViaFace.LBPvariants
 
 
         // get AMLBP pic
-        public List<int> AMLBPPic()
+        public Bitmap GetFiltered()
         {
-            // make up an answer 
-            Bitmap Filtered = new Bitmap(ThisPic.Width, ThisPic.Height);
+           
             // loop through one window and get the average 
             // goes through them all applies rules then the pixels that pass are marked as detected 
             int x, y, z;
@@ -56,39 +48,25 @@ namespace StressdetectionViaFace.LBPvariants
 
             }
             // return the answer 
-            return Lattice;
+            return ThisPic ;
         }
-        // lets eliminate duplicates 
-
-        public List<SanDictionaryItem> GetHistogram()
-        {
-            AMLBPPic();
-
-            List<SanDictionaryItem> items = new List<SanDictionaryItem>();
-            
-            var query = Lattice.GroupBy(x => x)
-              .Where(g => g.Count() > 1)
-              .Select(y => new  SanDictionaryItem(  y.Key ,  y.Count()) )
-              .ToList();
 
 
-            return query;
-        }
         // get AMLBP for a point 
-        private int AMLBPthis(int X,int Y)
+        private int AMLBPthis(int X, int Y)
         {
             double average = 0;
             int total = 0;
             double count = 1;
             // loop through radius distances
-            for(int x1 = 1;x1<=Radius; x1++)
+            for (int x1 = 1; x1 <= Radius; x1++)
             {
                 average = IntensityAverage(X, Y, x1);
                 total += GetPatternAround(X, Y, x1, average);
                 count += 1;
             }
 
-            return (int)Math.Truncate(total/count);
+            return (int)Math.Truncate(total / count);
         }
         // get average of pixels the same distance away from the point
         private double IntensityAverage(int X, int Y, int dist)
@@ -97,32 +75,32 @@ namespace StressdetectionViaFace.LBPvariants
             double total = 0;
             int counter = 0;
             // window around that pixel
-            for (int y1 = Y - (dist+1); y1 <= Y + (dist+1) ; y1++)
+            for (int y1 = Y - (dist + 1); y1 <= Y + (dist + 1); y1++)
             {
                 for (int x1 = X - (dist + 1); x1 <= X + (dist + 1); x1++)
                 {
                     // explicit bounds checks 
-                    if ((x1 >= 0) && (y1 >= 0) && (x1 <ThisPic .Width) && (y1 < ThisPic .Height))
+                    if ((x1 >= 0) && (y1 >= 0) && (x1 < ThisPic.Width) && (y1 < ThisPic.Height))
                     {
                         // check if its at the right distance 
                         // point A(centre) to B
                         Point A = new Point(X, Y);
                         Point B = new Point(x1, y1);
                         int distanceAtoB = DistanceAtoB(A, B);
-                        if(dist== distanceAtoB )
+                        if (dist == distanceAtoB)
                         {
                             // now add it to the total 
                             total += ThisPic.GetPixel(x1, y1).R;
                             counter += 1;
                         }
 
-                        
+
                     }
 
                 }
             }
 
-            return total / counter ;
+            return total / counter;
         }
         private int GetPatternAround(int X, int Y, int dist, double Average)
         {
@@ -144,41 +122,37 @@ namespace StressdetectionViaFace.LBPvariants
                         int distanceAtoB = DistanceAtoB(A, B);
                         if (dist == distanceAtoB)
                         {
-                            // this is on the angle
-                            if(ThisPic.GetPixel(x1,y1).R <ave )
-                            {
-                                // it needs to be a zero 
-                                Binary += "0";
-                            }
-                            else
-                            {
-                                // it need a  1 
-                                Binary += "1";
-                            }
+                            // it needs to be a zero 
+                            Color nc = Color.FromArgb(ave, ave, ave);
+                            ThisPic.SetPixel(x1, y1, nc);
+                           
+                         
                         }
                     }
 
                 }
             }
-            return GetDecimalFromBinary(Binary);
+            return 0;
+            //return GetDecimalFromBinary(Binary);
 
         }
         // binary string to decimal
-        private int GetDecimalFromBinary( string input)
+        private int GetDecimalFromBinary(string input)
         {
-           int output = Convert.ToInt32(input, 2);
+            int output = Convert.ToInt32(input, 2);
             return output;
         }
 
         // we need to check distance to see if its in the distance (between two points )
 
-        private int DistanceAtoB(Point A,Point B)
+        private int DistanceAtoB(Point A, Point B)
         {
             int answer = 0;
             // 1 is B
             // distance = root(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)))
-            answer = (int)Math.Truncate(Math.Sqrt(((B.X -A.X )*(B.X - A.X))+((B.Y  - A.Y ) * (B.Y - A.Y))));
+            answer = (int)Math.Truncate(Math.Sqrt(((B.X - A.X) * (B.X - A.X)) + ((B.Y - A.Y) * (B.Y - A.Y))));
             return answer;
         }
     }
 }
+

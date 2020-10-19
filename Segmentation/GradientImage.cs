583ecc10a038898970de224c98f3be9296d94821
Sanveer-Singh <sanveer001@gmail.com>
2020-10-19 09:45:59 +0200

    Gets the total gradient pixel

--- Segmentation/GradientImage.cs
+++ Segmentation/GradientImage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,130 @@ namespace StressdetectionViaFace.Segmentation
 {
     class GradientImage
     {
+        int height;
+        int width;
+        Bitmap greyscaled;
         // get a gradient bitmap from a normal greyscaled bitmap 
+
+      
+        // get total edge pixel 
+        public double TotalEdgePixel(int x, int y)
+        {
+            double ygradient = yGradient(x, y);
+            double xgradient = XGradient(x, y);
+
+            return Math.Sqrt((xgradient * xgradient) + (ygradient * ygradient));
+        }
+
+        /// <summary>
+        /// sobel style gradient for X getting 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public double XGradient(int x, int y)
+        {
+            List<double> values = new List<double>();
+            for (int y1 = y - 1; y1 <= y + 1; y1++)
+            {
+                for (int x1 = x - 1; x1 <= x + 1; x1++)
+                {
+                    values.Add(getValueAt(x, y));
+                }
+            }
+
+            values[0] = values[0] * -1;
+            values[1] = values[1] * 0;
+            values[1] = values[1] * -1;
+            values[2] = values[2] * -2;
+            values[3] = values[3] * 0;
+            values[4] = values[4] * 2;
+            values[5] = values[5] * -1;
+            values[6] = values[6] * 0;
+            values[7] = values[7] * 1;
+
+            double total = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                total += values[i];
+            }
+            return total;
+        }
+
+        /// <summary>
+        /// sobel style gradient getting 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public double yGradient(int x,int y)
+        {
+            List<double> values = new List<double>();
+             for(int y1 = y-1; y1<=y+1;y1++)
+            {
+                 for(int x1 = x-1;x1<=x+1;x1++)
+                {
+                    values.Add(getValueAt(x, y));
+                }
+            }
+
+            values[0] = values[0] * 1;
+            values[1] = values[1] * 2;
+            values[1] = values[1] * 1 ;
+            values[2] = values[2] * 0;
+            values[3] = values[3] * 0;
+            values[4] = values[4] * 0;
+            values[5] = values[5] * -1;
+            values[6] = values[6] * -2;
+            values[7] = values[7] * -1;
+
+            double total=0;
+            for(int i =0; i<8;i++)
+            {
+                total += values[i];
+            }
+            return total;
+        }
+         
+        public double getValueAt(int x, int y)
+        {
+            double val = 0;
+            if(InXBounds(x)&& InYBounds(y))
+            {
+               val= greyscaled.GetPixel(x, y).R ;
+            }
+            return val;
+        }
+
+        //
+        /// <summary>
+        /// gets if an x value is in the bounds 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public bool InXBounds(int x)
+        {
+            bool answer = false;
+            if ((x>0)&& (x<width))
+            {
+                answer = true;
+            }
+            return answer;
+        }
+        /// <summary>
+        /// checks if Y value is in bounds 
+        /// </summary>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public bool InYBounds(int y)
+        {
+            bool answer = false;
+            if ((y > 0) && (y < height))
+            {
+                answer = true;
+            }
+            return answer;
+        }
+
     }
 }

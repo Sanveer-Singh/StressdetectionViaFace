44bdc5b34722fee6095feae8f6e5a51f2bfc34b6
Sanveer-Singh <sanveer001@gmail.com>
2021-01-19 16:13:05 +0200

    Pipeline basic setup achieved

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,17 +29,14 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.btnChooseImage = new System.Windows.Forms.Button();
             this.picBox = new System.Windows.Forms.PictureBox();
             this.GetDetectedFace = new System.Windows.Forms.Button();
             this.FillPatches = new System.Windows.Forms.Button();
-            this.btnGetFace = new System.Windows.Forms.Button();
             this.btnGreyScalar = new System.Windows.Forms.Button();
             this.btnGetFaceViaLibrary = new System.Windows.Forms.Button();
             this.btnFilter = new System.Windows.Forms.Button();
             this.btnLBP = new System.Windows.Forms.Button();
             this.btnADLBP = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -49,78 +46,64 @@
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.GradientTest = new System.Windows.Forms.Button();
             this.NI_LBP = new System.Windows.Forms.Button();
+            this.btnPipeline = new System.Windows.Forms.Button();
+            this.btnPipeline2 = new System.Windows.Forms.Button();
+            this.btnPipeline3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnChooseImage = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnChooseImage
-            // 
-            this.btnChooseImage.Location = new System.Drawing.Point(31, 70);
-            this.btnChooseImage.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
-            this.btnChooseImage.Name = "btnChooseImage";
-            this.btnChooseImage.Size = new System.Drawing.Size(114, 39);
-            this.btnChooseImage.TabIndex = 0;
-            this.btnChooseImage.Text = "OpenImage";
-            this.btnChooseImage.UseVisualStyleBackColor = true;
-            this.btnChooseImage.Click += new System.EventHandler(this.btnChooseImage_Click);
-            // 
             // picBox
             // 
-            this.picBox.Location = new System.Drawing.Point(259, 70);
-            this.picBox.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.picBox.Location = new System.Drawing.Point(170, 107);
+            this.picBox.Margin = new System.Windows.Forms.Padding(1);
             this.picBox.Name = "picBox";
-            this.picBox.Size = new System.Drawing.Size(631, 467);
+            this.picBox.Size = new System.Drawing.Size(343, 294);
             this.picBox.TabIndex = 1;
             this.picBox.TabStop = false;
             // 
             // GetDetectedFace
             // 
             this.GetDetectedFace.Location = new System.Drawing.Point(31, 138);
-            this.GetDetectedFace.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.GetDetectedFace.Margin = new System.Windows.Forms.Padding(1);
             this.GetDetectedFace.Name = "GetDetectedFace";
             this.GetDetectedFace.Size = new System.Drawing.Size(114, 40);
             this.GetDetectedFace.TabIndex = 2;
             this.GetDetectedFace.Text = "Show skin detected";
             this.GetDetectedFace.UseVisualStyleBackColor = true;
+            this.GetDetectedFace.Visible = false;
             this.GetDetectedFace.Click += new System.EventHandler(this.GetDetectedFace_Click);
             // 
             // FillPatches
             // 
             this.FillPatches.Location = new System.Drawing.Point(31, 186);
-            this.FillPatches.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.FillPatches.Margin = new System.Windows.Forms.Padding(1);
             this.FillPatches.Name = "FillPatches";
             this.FillPatches.Size = new System.Drawing.Size(114, 41);
             this.FillPatches.TabIndex = 3;
             this.FillPatches.Text = "Fill patches";
             this.FillPatches.UseVisualStyleBackColor = true;
+            this.FillPatches.Visible = false;
             this.FillPatches.Click += new System.EventHandler(this.FillPatches_Click);
-            // 
-            // btnGetFace
-            // 
-            this.btnGetFace.Location = new System.Drawing.Point(31, 340);
-            this.btnGetFace.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
-            this.btnGetFace.Name = "btnGetFace";
-            this.btnGetFace.Size = new System.Drawing.Size(114, 47);
-            this.btnGetFace.TabIndex = 4;
-            this.btnGetFace.Text = "Display face only*";
-            this.btnGetFace.UseVisualStyleBackColor = true;
-            this.btnGetFace.Click += new System.EventHandler(this.btnGetFace_Click);
             // 
             // btnGreyScalar
             // 
-            this.btnGreyScalar.Location = new System.Drawing.Point(31, 241);
-            this.btnGreyScalar.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.btnGreyScalar.Location = new System.Drawing.Point(-200, -547);
+            this.btnGreyScalar.Margin = new System.Windows.Forms.Padding(1);
             this.btnGreyScalar.Name = "btnGreyScalar";
             this.btnGreyScalar.Size = new System.Drawing.Size(114, 47);
             this.btnGreyScalar.TabIndex = 5;
             this.btnGreyScalar.Text = "Apply greyscaling";
             this.btnGreyScalar.UseVisualStyleBackColor = true;
+            this.btnGreyScalar.Visible = false;
             this.btnGreyScalar.Click += new System.EventHandler(this.btnGreyScalar_Click);
             // 
             // btnGetFaceViaLibrary
             // 
             this.btnGetFaceViaLibrary.Location = new System.Drawing.Point(921, 70);
-            this.btnGetFaceViaLibrary.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.btnGetFaceViaLibrary.Margin = new System.Windows.Forms.Padding(1);
             this.btnGetFaceViaLibrary.Name = "btnGetFaceViaLibrary";
             this.btnGetFaceViaLibrary.Size = new System.Drawing.Size(114, 49);
             this.btnGetFaceViaLibrary.TabIndex = 6;
@@ -131,7 +114,7 @@
             // btnFilter
             // 
             this.btnFilter.Location = new System.Drawing.Point(31, 439);
-            this.btnFilter.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.btnFilter.Margin = new System.Windows.Forms.Padding(1);
             this.btnFilter.Name = "btnFilter";
             this.btnFilter.Size = new System.Drawing.Size(114, 44);
             this.btnFilter.TabIndex = 7;
@@ -142,7 +125,7 @@
             // btnLBP
             // 
             this.btnLBP.Location = new System.Drawing.Point(28, 619);
-            this.btnLBP.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.btnLBP.Margin = new System.Windows.Forms.Padding(1);
             this.btnLBP.Name = "btnLBP";
             this.btnLBP.Size = new System.Drawing.Size(114, 32);
             this.btnLBP.TabIndex = 10;
@@ -153,23 +136,13 @@
             // btnADLBP
             // 
             this.btnADLBP.Location = new System.Drawing.Point(28, 665);
-            this.btnADLBP.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.btnADLBP.Margin = new System.Windows.Forms.Padding(1);
             this.btnADLBP.Name = "btnADLBP";
             this.btnADLBP.Size = new System.Drawing.Size(114, 36);
             this.btnADLBP.TabIndex = 11;
             this.btnADLBP.Text = "Get ADLBP histogram";
             this.btnADLBP.UseVisualStyleBackColor = true;
             this.btnADLBP.Click += new System.EventHandler(this.btnADLBP_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 33);
-            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 15);
-            this.label1.TabIndex = 12;
-            this.label1.Text = "Get The image:";
             // 
             // label2
             // 
@@ -180,6 +153,7 @@
             this.label2.Size = new System.Drawing.Size(125, 15);
             this.label2.TabIndex = 13;
             this.label2.Text = "Localization By Hand:";
+            this.label2.Visible = false;
             // 
             // label3
             // 
@@ -204,7 +178,7 @@
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(31, 497);
-            this.button2.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.button2.Margin = new System.Windows.Forms.Padding(1);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(113, 36);
             this.button2.TabIndex = 16;
@@ -215,7 +189,7 @@
             // picBox2
             // 
             this.picBox2.Location = new System.Drawing.Point(954, 168);
-            this.picBox2.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.picBox2.Margin = new System.Windows.Forms.Padding(1);
             this.picBox2.Name = "picBox2";
             this.picBox2.Size = new System.Drawing.Size(270, 203);
             this.picBox2.TabIndex = 18;
@@ -224,7 +198,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(24, 712);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(1);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(340, 160);
@@ -256,11 +230,67 @@
             this.NI_LBP.UseVisualStyleBackColor = true;
             this.NI_LBP.Click += new System.EventHandler(this.NI_LBP_Click);
             // 
+            // btnPipeline
+            // 
+            this.btnPipeline.Location = new System.Drawing.Point(170, 12);
+            this.btnPipeline.Name = "btnPipeline";
+            this.btnPipeline.Size = new System.Drawing.Size(192, 46);
+            this.btnPipeline.TabIndex = 22;
+            this.btnPipeline.Text = "Classify with Pipeline1";
+            this.btnPipeline.UseVisualStyleBackColor = true;
+            this.btnPipeline.Click += new System.EventHandler(this.btnPipeline_Click);
+            // 
+            // btnPipeline2
+            // 
+            this.btnPipeline2.Location = new System.Drawing.Point(410, 12);
+            this.btnPipeline2.Name = "btnPipeline2";
+            this.btnPipeline2.Size = new System.Drawing.Size(192, 46);
+            this.btnPipeline2.TabIndex = 23;
+            this.btnPipeline2.Text = "Classify with Pipeline2";
+            this.btnPipeline2.UseVisualStyleBackColor = true;
+            this.btnPipeline2.Click += new System.EventHandler(this.btnPipeline2_Click);
+            // 
+            // btnPipeline3
+            // 
+            this.btnPipeline3.Location = new System.Drawing.Point(661, 12);
+            this.btnPipeline3.Name = "btnPipeline3";
+            this.btnPipeline3.Size = new System.Drawing.Size(192, 46);
+            this.btnPipeline3.TabIndex = 24;
+            this.btnPipeline3.Text = "Classify with Pipeline3";
+            this.btnPipeline3.UseVisualStyleBackColor = true;
+            this.btnPipeline3.Click += new System.EventHandler(this.btnPipeline3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 15);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Get The image:";
+            this.label1.Visible = false;
+            // 
+            // btnChooseImage
+            // 
+            this.btnChooseImage.Location = new System.Drawing.Point(31, 70);
+            this.btnChooseImage.Margin = new System.Windows.Forms.Padding(1);
+            this.btnChooseImage.Name = "btnChooseImage";
+            this.btnChooseImage.Size = new System.Drawing.Size(114, 39);
+            this.btnChooseImage.TabIndex = 0;
+            this.btnChooseImage.Text = "OpenImage";
+            this.btnChooseImage.UseVisualStyleBackColor = true;
+            this.btnChooseImage.Visible = false;
+            this.btnChooseImage.Click += new System.EventHandler(this.btnChooseImage_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1227, 884);
+            this.Controls.Add(this.btnPipeline3);
+            this.Controls.Add(this.btnPipeline2);
+            this.Controls.Add(this.btnPipeline);
             this.Controls.Add(this.NI_LBP);
             this.Controls.Add(this.GradientTest);
             this.Controls.Add(this.textBox1);
@@ -275,12 +305,11 @@
             this.Controls.Add(this.btnFilter);
             this.Controls.Add(this.btnGetFaceViaLibrary);
             this.Controls.Add(this.btnGreyScalar);
-            this.Controls.Add(this.btnGetFace);
             this.Controls.Add(this.FillPatches);
             this.Controls.Add(this.GetDetectedFace);
             this.Controls.Add(this.picBox);
             this.Controls.Add(this.btnChooseImage);
-            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.Margin = new System.Windows.Forms.Padding(1);
             this.Name = "Form1";
             this.Text = "PreProccessing";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -292,18 +321,14 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnChooseImage;
         private System.Windows.Forms.PictureBox picBox;
         private System.Windows.Forms.Button GetDetectedFace;
         private System.Windows.Forms.Button FillPatches;
-        private System.Windows.Forms.Button btnGetFace;
         private System.Windows.Forms.Button btnGreyScalar;
         private System.Windows.Forms.Button btnGetFaceViaLibrary;
         private System.Windows.Forms.Button btnFilter;
         private System.Windows.Forms.Button btnLBP;
         private System.Windows.Forms.Button btnADLBP;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
@@ -313,6 +338,11 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.Button GradientTest;
         private System.Windows.Forms.Button NI_LBP;
+        private System.Windows.Forms.Button btnPipeline;
+        private System.Windows.Forms.Button btnPipeline2;
+        private System.Windows.Forms.Button btnPipeline3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnChooseImage;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,7 @@ using StressdetectionViaFace.Preprocessing;
 using StressdetectionViaFace.LBPvariants;
 using StressdetectionViaFace.utilities;
 using StressdetectionViaFace.Segmentation;
+using Microsoft.VisualBasic;
 
 namespace StressdetectionViaFace
 {
@@ -30,16 +31,10 @@ namespace StressdetectionViaFace
         public Bitmap MyGradientImage;
         // save the face only
         public Bitmap FaceOnly;
+        public string FileName1;
+        public string FileLocation1;
 
-        // histograms
-        string histogram1a = null;
-        string histogram1b = "0:0;1:5452;2:9880;3:10321;4:62480;5:1020;6:1780;7:873;8:391;9:275;10:1264;11:1645;12:173;13:2455;14:86;15:125;16:77;17:621;18:218;19:60;20:50;21:75;22:1577;23:465;24:82;25:193;26:130;27:15;28:14;29:6;30:13;31:11;32:83;33:83;34:119;35:24;36:11;37:6;38:34;39:18;40:14;41:37;42:28;43:41;44:13;45:1012;46:40;47:53;48:40;49:1981;50:78;51:61;52:21;53:20;54:2499;55:349;56:33;57:234;58:81;59:24;60:8;61:9;62:21;63:26;64:161;65:102;66:58;67:20;68:26;69:55;70:595;71:244;72:29;73:39;74:133;75:17;76:11;77:8;78:104;79:94;80:26;81:53;82:123;83:52;84:19;85:72;86:133;87:42;88:40;89:21;90:25;91:22;92:26;93:41;94:47;95:123;96:1036;97:103;98:3;99:49;100:5;101:6;102:10;103:58;104:12;105:15;106:50;107:22;108:8;109:106;110:27;111:37;112:11;113:2090;114:38;115:37;116:10;117:17;118:12;119:23;120:19;121:922;122:37;123:36;124:8;125:31;126:8;127:44;128:57;129:55;130:4;131:36;132:2;133:48;134:0;135:55;136:6;137:68;138:24;139:11;140:4;141:4;142:8;143:31;144:21;145:0;146:0;147:0;148:0;149:5;150:0;151:6;152:0;153:0;154:1;155:3;156:0;157:5;158:5;159:26;160:10;161:243;162:3;163:59;164:1;165:53;166:4;167:199;168:5;169:13;170:40;171:6;172:4;173:1;174:0;175:11;176:8;177:0;178:0;179:1;180:2;181:7;182:2;183:12;184:3;185:9;186:3;187:6;188:1;189:18;190:6;191:29;192:61;193:3;194:0;195:44;196:8;197:21;198:3;199:80;200:0;201:0;202:0;203:0;204:2;205:0;206:1;207:1;208:0;209:0;210:1;211:8;212:5;213:2;214:3;215:1;216:0;217:6;218:0;219:10;220:0;221:4;222:1;223:13;224:7;225:8;226:8;227:67;228:2;229:35;230:5;231:52;232:1;233:4;234:6;235:4;236:3;237:12;238:7;239:10;240:6;241:326;242:34;243:77;244:3;245:1;246:10;247:35;248:11;249:2358;250:44;251:26;252:4;253:6;254:14;255:17;";
-        string histogram2a = null;
-        string histogram3a = null;
-        string histogram2b ="0:26;1:27;2:28;3:148;4:617;5:1046;6:68;7:6550;8:483;9:7;10:360;11:7052;12:1196;13:196;14:258;15:8452;16:0;17:0;18:1;19:0;20:1230;21:4057;22:0;23:2789;24:2;25:0;26:0;27:2;28:139;29:55;30:9;31:485;32:23;33:5;34:0;35:4;36:0;37:0;38:0;39:1;40:3;41:0;42:1;43:5;44:0;45:0;46:0;47:1;48:13;49:1;50:0;51:1;52:3824;53:2980;54:0;55:1420;56:4;57:0;58:0;59:5;60:240;61:102;62:4;63:477;64:0;65:0;66:0;67:0;68:0;69:0;70:0;71:7;72:324;73:0;74:897;75:6755;76:142;77:5;78:82;79:893;80:0;81:0;82:0;83:0;84:0;85:0;86:0;87:0;88:0;89:0;90:0;91:0;92:4;93:2;94:0;95:8;96:17;97:3;98:0;99:1;100:1;101:0;102:0;103:5;104:953;105:1;106:2778;107:4428;108:179;109:10;110:99;111:902;112:40;113:0;114:0;115:2;116:6370;117:1382;118:1;119:1036;120:7089;121:2;122:2521;123:2630;124:9211;125:674;126:775;127:26478;128:0;129:0;130:0;131:0;132:0;133:0;134:0;135:0;136:0;137:0;138:0;139:0;140:0;141:0;142:0;143:0;144:0;145:0;146:0;147:0;148:0;149:0;150:0;151:0;152:0;153:0;154:0;155:0;156:0;157:0;158:0;159:0;160:0;161:0;162:0;163:0;164:0;165:0;166:0;167:0;168:0;169:0;170:0;171:0;172:0;173:0;174:0;175:0;176:0;177:0;178:0;179:0;180:0;181:0;182:0;183:0;184:0;185:0;186:0;187:0;188:0;189:0;190:0;191:0;192:0;193:0;194:0;195:0;196:0;197:0;198:0;199:0;200:0;201:0;202:0;203:0;204:0;205:0;206:0;207:0;208:0;209:0;210:0;211:0;212:0;213:0;214:0;215:0;216:0;217:0;218:0;219:0;220:0;221:0;222:0;223:0;224:0;225:0;226:0;227:0;228:0;229:0;230:0;231:0;232:0;233:0;234:0;235:0;236:0;237:0;238:0;239:0;240:0;241:0;242:0;243:0;244:0;245:0;246:0;247:0;248:0;249:0;250:0;251:0;252:0;253:0;254:0;255:0;";
-        string histogram3b = "0:3;1:8572;2:14830;3:9130;4:60230;5:1678;6:2267;7:724;8:104;9:117;10:1512;11:1490;12:62;13:2339;14:77;15:52;16:8;17:290;18:60;19:38;20:36;21:43;22:1913;23:242;24:42;25:135;26:36;27:18;28:10;29:14;30:10;31:3;32:24;33:5;34:9;35:3;36:1;37:2;38:5;39:3;40:2;41:35;42:12;43:18;44:8;45:718;46:18;47:15;48:28;49:1683;50:16;51:15;52:6;53:7;54:2056;55:311;56:11;57:97;58:36;59:12;60:3;61:7;62:4;63:2;64:11;65:10;66:6;67:4;68:1;69:1;70:317;71:90;72:9;73:32;74:40;75:7;76:3;77:3;78:97;79:40;80:35;81:16;82:62;83:30;84:44;85:68;86:28;87:36;88:28;89:10;90:5;91:9;92:17;93:26;94:44;95:172;96:1142;97:4;98:2;99:6;100:2;101:2;102:0;103:12;104:0;105:3;106:2;107:4;108:2;109:28;110:3;111:9;112:7;113:1851;114:12;115:11;116:2;117:3;118:1;119:1;120:4;121:989;122:16;123:4;124:5;125:0;126:1;127:0;128:14;129:0;130:0;131:6;132:0;133:4;134:1;135:3;136:0;137:25;138:13;139:12;140:5;141:1;142:2;143:1;144:8;145:0;146:0;147:0;148:0;149:0;150:0;151:1;152:0;153:0;154:0;155:0;156:0;157:0;158:0;159:11;160:0;161:0;162:7;163:0;164:0;165:6;166:3;167:22;168:0;169:1;170:1;171:3;172:2;173:0;174:1;175:0;176:6;177:0;178:0;179:1;180:1;181:2;182:1;183:0;184:0;185:0;186:10;187:2;188:1;189:5;190:1;191:5;192:15;193:1;194:1;195:4;196:0;197:0;198:0;199:89;200:0;201:0;202:3;203:2;204:0;205:0;206:1;207:3;208:0;209:2;210:10;211:9;212:4;213:1;214:2;215:2;216:6;217:2;218:3;219:10;220:1;221:3;222:4;223:16;224:0;225:3;226:3;227:25;228:2;229:6;230:3;231:12;232:0;233:1;234:1;235:0;236:1;237:6;238:2;239:4;240:1;241:144;242:5;243:37;244:1;245:0;246:1;247:19;248:1;249:3720;250:16;251:12;252:5;253:0;254:0;255:5;";
-
-
+     
 
 
         public Form1()
@@ -49,7 +44,22 @@ namespace StressdetectionViaFace
 
         private void Form1_Load(object sender, EventArgs e)
         {
-           
+            //HYPKNNDataContext db = new HYPKNNDataContext();
+            //Pipeline p = new Pipeline();
+            //p.CONCATpattern = "test1";
+            //p.CONCATpredicted = "test1";
+            //p.LABEL = "test";
+            //p.lOCATION = "test";
+            //p.NAME = "test";
+            //db.Pipelines.InsertOnSubmit(p);
+            //db.SubmitChanges();
+            //Pipeline x = new Pipeline();
+            //var test = (from u in db.Pipelines
+            //           where u.CONCATpattern == "test1"
+            //           select u).FirstOrDefault();
+
+            //x = (Pipeline)test;
+       
         }
 
         private void btnChooseImage_Click(object sender, EventArgs e)
@@ -65,13 +75,16 @@ namespace StressdetectionViaFace
                 // set size
                 Size sz = new Size(500,500);
                 // display image in picture box  
-                Bitmap m1 = new Bitmap(open.FileName);
+                // image file path  
+                FileLocation1 = open.FileName;
+                FileName1 = Interaction.InputBox("Please enter file name ");
+                 Bitmap m1 = new Bitmap(FileLocation1);
                 Image img = new Bitmap(m1);
                 original = m1;
                 Bitmap bp = new Bitmap(img,sz);
                 
                 picBox.Image = bp;
-                // image file path  
+             
             }
         }
 
@@ -105,9 +118,9 @@ namespace StressdetectionViaFace
         }
 
         // message box function 
-        public void msgBox()
+        public void msgBox( string Input= "its finaly done")
         {
-            string message = "Finally done";
+            string message = Input;
             string caption = "its done";
            
              MessageBox.Show(message, caption);
@@ -195,18 +208,26 @@ namespace StressdetectionViaFace
 
         private void btnLBP_Click(object sender, EventArgs e)
         {
+            LBP();
+
+        }
+
+        private string LBP()
+        {
+            string temp = "";
             LBP lbp = new LBP(original);
             List<SanDictionaryItem> histogram = lbp.GetHistogram();
-            string temp = "";
+          
             foreach (SanDictionaryItem i in histogram)
             {
-                temp += i.ToString() ;
+                temp += i.ToString();
             }
             textBox1.Text = temp;
-            MessageBox.Show(temp, "histogram");
+            //MessageBox.Show(temp, "histogram");
             Size sz = new Size(original.Width, original.Height);
             Image img = new Bitmap(lbp.GetPic());
             picBox.Image = new Bitmap(img, sz);
+            return temp;
 
         }
 
@@ -278,5 +299,79 @@ namespace StressdetectionViaFace
             Image img = new Bitmap(lbp.GetPic());
             picBox.Image = new Bitmap(img, sz);
         }
+
+        private string nilbp()
+        {
+            NI_LBP lbp = new NI_LBP(original);
+            List<SanDictionaryItem> histogram = lbp.GetHistogram();
+            string temp = "";
+            foreach (SanDictionaryItem i in histogram)
+            {
+                temp += i.ToString();
+            }
+            textBox1.Text = temp;
+            //MessageBox.Show(temp, "histogram");
+            Size sz = new Size(original.Width, original.Height);
+            Image img = new Bitmap(lbp.GetPic());
+            picBox.Image = new Bitmap(img, sz);
+            return temp;
+        }
+
+        private void btnPipeline_Click(object sender, EventArgs e)
+        {
+            btnChooseImage_Click(sender, e);
+            // step 1
+            string input = Interaction.InputBox("Please enter the true label");
+            GetDetectedFace_Click(sender, e);
+           // step 2
+            FillPatches_Click(sender, e);
+            // step 3
+            btnGreyScalar_Click(sender, e);
+            btnFilter_Click(sender, e);
+            // step 4
+            string hist = LBP();
+        
+
+
+        }
+
+        private void btnPipeline2_Click(object sender, EventArgs e)
+        {
+            btnChooseImage_Click(sender, e);
+            // step 1
+            string input = Interaction.InputBox("Please enter the true label");
+            // step 2
+            btnGetFaceViaLibrary_Click(sender, e);
+            
+            // step 3
+
+            btnFilter_Click(sender, e);
+            // step 4
+            string hist = nilbp();
+        }
+
+        private void btnPipeline3_Click(object sender, EventArgs e)
+        {
+            btnChooseImage_Click(sender, e);
+            // step 1
+            string input = Interaction.InputBox("Please enter the true label");
+            // step 2
+            btnGetFaceViaLibrary_Click(sender, e);
+
+            // step 3
+
+            btnFilter_Click(sender, e);
+            // step 4
+            string hist1 = nilbp();
+            Bitmap m1 = new Bitmap(FileLocation1);
+            Image img = new Bitmap(m1);
+            btnGetFaceViaLibrary_Click(sender, e);
+            btnFilter_Click(sender, e);
+            string hist2 = LBP();
+            string hist = hist1 + hist2;
+
+        }
+
+
     }
 }
